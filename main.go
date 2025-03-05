3e2c42e5901ff4d8a32458a6af0bf6af39ef7852
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-05 16:49:43 +0100

    Create expense list variable and load from the file

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+
+	"github.com/hayohtee/expense-tracker/internal/expense"
 )
+
+const filename = ".expense_list.json"
 
 func main() {
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
@@ -14,5 +19,12 @@ func main() {
 	if len(os.Args) < 2 {
 		flag.Usage()
 		os.Exit(0)
+	}
+	
+	// Load the expense list from the file.
+	var expenseList expense.ExpenseList
+	if err := expenseList.Load(filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
