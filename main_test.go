c79faa218b11881950f1ec37c3bcc42089424a19
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-05 18:18:15 +0100

    Add test for add subcommand

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"testing"
 )
@@ -21,9 +22,9 @@ func TestMain(m *testing.M) {
 		binName += ".exe"
 	}
 
-	build := exec.Command("go", "build", "-o", binName, "./...")
+	build := exec.Command("go", "build", "-o", binName, "main.go")
 	if err := build.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "cannot build tool %s:%s", binName, err.Error())
+		fmt.Fprintf(os.Stderr, "cannot build tool %s:%s\n", binName, err.Error())
 		os.Exit(1)
 	}
 
@@ -34,4 +35,40 @@ func TestMain(m *testing.M) {
 	os.Remove(filename)
 
 	os.Exit(result)
+}
+
+func TestExpenseTracker(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	expenses := []struct {
+		description string
+		amount      float64
+	}{
+		{description: "new expense 1", amount: 20.00},
+		{description: "new expense 2", amount: 20.60},
+		{description: "new expense 3", amount: 200.50},
+		{description: "new expense 4", amount: 200.00},
+		{description: "new expense 5", amount: 120.00},
+	}
+
+	t.Run("TestAddCMD", func(t *testing.T) {
+		for index, item := range expenses {
+			cmd := exec.Command(cmdPath, "add", "--description", item.description, "--amount", fmt.Sprint(item.amount))
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			expected := fmt.Sprintf("Expense added successfully (ID: %d)\n", index + 1)
+			if string(out) != expected {
+				t.Errorf("expected %q, but got %q instead", expected, string(out))
+			}
+		}
+	})
+
 }
