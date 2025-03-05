8911874075bab439e5d04b1c994c248ce6384a77
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-05 17:31:11 +0100

    Display usage information of the subcommands if no arguments is specified

--- main.go
+++ main.go
@@ -16,8 +16,13 @@ func main() {
 	summaryCmd := flag.NewFlagSet("summary", flag.ExitOnError)
 	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
 
+	description := addCmd.String("description", "", "The description for the expense")
+	amount := addCmd.Float64("amount", 0.0, "The amount for the expense")
+	month := summaryCmd.Int("month", 0, "The month to generate the summary for")
+	id := deleteCmd.Int("id", 0, "The ID of the expense to delete")
+
 	if len(os.Args) < 2 {
-		flag.Usage()
+		displayUsage(addCmd, summaryCmd, deleteCmd)
 		os.Exit(0)
 	}
 
@@ -30,8 +35,6 @@ func main() {
 
 	switch os.Args[1] {
 	case "add":
-		description := addCmd.String("description", "", "The description for the expense")
-		amount := addCmd.Float64("amount", 0.0, "The amount for the expense")
 		if err := addCmd.Parse(os.Args[2:]); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -59,7 +62,6 @@ func main() {
 		// Write the list of expense to the STDOUT.
 		expenseList.List(os.Stdout)
 	case "summary":
-		month := summaryCmd.Int("month", 0, "The month to generate the summary for")
 		if err := summaryCmd.Parse(os.Args[2:]); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -79,12 +81,11 @@ func main() {
 			os.Exit(1)
 		}
 	case "delete":
-		id := deleteCmd.Int("id", 0, "The ID of the expense to delete")
 		if err := deleteCmd.Parse(os.Args[2:]); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		
+
 		// Delete an expense from the list.
 		if err := expenseList.Delete(*id); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -92,11 +93,17 @@ func main() {
 		}
 		// Write success message to STDOUT.
 		fmt.Println("Expense deleted successfully")
-		
+
 		// Save the new list.
 		if err := expenseList.Save(filename); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	}
+}
+
+func displayUsage(flagSets ...*flag.FlagSet) {
+	for _, flagSet := range flagSets {
+		flagSet.Usage()
 	}
 }
